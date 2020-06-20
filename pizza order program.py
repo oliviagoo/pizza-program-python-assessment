9237d2417b9494705d0cda2a20937809bd5843d1
Olivia Goodman <oliviagoodman@Olivias-Air-3.hub>
2020-06-21 11:36:49 +1200

    Version Seven
    In this version I added the name input and a confirmation process at the end of the program. The name input is done at the start of the program (and if the user edits their delivery details later), and the confirmation process allows the user to change their pizza order and delivery details before they submit the order.

--- pizza order program.py
+++ pizza order program.py
@@ -1,16 +1,59 @@
 #pizza order program
-#olivia goodman 18/6/20
-#version 6 - adding the delivery option
+#olivia goodman 21/6/20
+#version 7 - adding extra details - confirmation, name
+
+#this function confirms the order and finishes the program with a final output
+def confirm(order, stuff_crust, price, order_type, address, name):
+    print("Thank you for your order! Please confirm that the following details are correct.")
+    while True:
+        output_order(price, order, stuff_crust)
+        correct = input("Is this what you would like to order? (Y or N) ").strip().lower()
+        if correct == "n":
+            reorder = input("Are you sure you want to redo your order? (Y or N) ")
+            if reorder == "y":
+                if order_type == "d":
+                    price = 8
+                else:
+                    price = 0
+                price, order, stuff_crust = order_input(price)
+        else: break
+    print()
+    while True:
+        delivery_output(order_type, address, name)
+        correct = input("Is this correct? (Y or N) ").strip().lower()
+        if correct == "n":
+            reorder = input("Are you sure you want to change your order details? (Y or N) ")
+            if reorder == "y":
+                if order_type == "d":
+                    price -= 8
+                order_type, address, price, name = delivery(price)
+        else: break
+    print()
+    print("Final order:")
+    delivery_output(order_type, address, name)
+    output_order(price, order, stuff_crust)
+    print("Thank you! See you soon, we hope you enjoy your meal!")
+
+#this function outputs the delivery/pick up information 
+def delivery_output(order_type, address, name):
+    if order_type == "d":
+        print("Order Delivery")
+        print("Delivery address: {}".format(address))
+        print("Delivered to: {}".format(name.title()))
+    else:
+        print("Order Pickup")
+        print("Picked up by: {}".format(name.title()))
 
 #this function asks the user whether they want delivery or pickup
 def delivery(price):
+    name = input("What is your name? ")
     answer = input("Would you like to get your pizza delivered (D), or pick it up? (P) ").lower().strip()
     if answer == "d":
         price += 8
         address = input("What is your delivery address? ")
-        return answer, address, price
+        return answer, address, price, name
     else:
-        return answer, "n/a", price
+        return answer, "n/a", price, name
 
 #this function outputs the menu
 def output_menu():
@@ -26,7 +69,7 @@ def output_menu():
     print()
 
 #this function orders a pizza
-def order_pizza(price):
+def order_pizza(price, order, stuff_crust):
     pizza_order = int(input("Enter the ID of the pizza you want to order: "))
     order.append(pizza_order)
     if pizza_order < gourmet_menu[0][2]:
@@ -34,12 +77,12 @@ def order_pizza(price):
     else:
         price += 15
     print()
-    answer, price = stuffed_crust(price)
+    answer, price = stuffed_crust(price, stuff_crust)
     stuff_crust.append(answer)
-    return price
+    return price, order
 
 #this function asks if the user wants stuffed crust
-def stuffed_crust(price):
+def stuffed_crust(price, stuff_crust):
     answer = input("Would you like to add stuffed crust for $3? (Y or N?) ").lower().strip()
     if answer == "y":
         price += 3
@@ -47,7 +90,7 @@ def stuffed_crust(price):
     return answer, price
     
 #this function outputs what the user has ordered so far, and the total price
-def output_order(price):
+def output_order(price, order, stuff_crust):
     print("Your order: ")
     for pizza in range(len(order)):
         if stuff_crust[pizza] == "y":
@@ -77,30 +120,32 @@ def output_order(price):
 
 #this function runs the main menu for the input
 def order_input(price):
+    #everything the user orders will get added to this list
+    order = []
+    #keeps track of which pizzas have stuffed crust
+    stuff_crust = []
     while True:
         selection = input("Enter M to see the menu, P to order a pizza, O to see your order so far, or Q to quit: ").lower().strip()
         print()
         if selection == "m":
             output_menu()
         elif selection == "p":
-            price = order_pizza(price)
+            price, order = order_pizza(price, order, stuff_crust)
         elif selection == "o":
-            output_order(price)
+            output_order(price, order, stuff_crust)
         else:
             break
-    return price
+    return price, order, stuff_crust
         
 #main routine
 
 #menu lists - storing the pizzas the user can choose from
 regular_menu = [["Margherita", "Fresh tomato, mozzarella, fresh basil, parmesan", 1], ["Kiwi", "Bacon, egg, mozzarella", 2], ["Garlic", "Mozzarella, garlic", 3], ["Cheese", "Mozzarella, oregano", 4], ["Hawaiian", "Ham, pineapple, mozzarella", 5], ["Mediterranean (vegan)", "Lebanese herbs, olive oil, fresh tomatoes, olives, onion", 6]]
 gourmet_menu = [["Meat", "Bacon, pancetta, ham, onion, pepperoni, mozzarella", 7], ["Chicken Cranberry", "Smoked chicken, cranberry, camembert mozzarella", 8], ["Satay Chicken", "Smoked chic, onions, capsicum, pine nuts, satay sauce, mozzarella Chilli flakes and dried basil", 9], ["Big BBQ Bacon", "Smoky Bacon served on our classic marinara tomato sauce, heaped with mozzarella, topped off with a sweet and tangy BBQ drizzle", 10], ["Veggie", "Sweet red onion, mushroom, red capsicum & melting mozzarella with drizzles of our tangy roast capsicum drizzle, finished with a dash of oregano", 11], ["Meatlovers", "Spicy pepperoni, Italian sausage, succulent ham, seasoned ground beef, and crispy bacon all piled onto classic marinara sauce and finished with cheesy mozzarella and a drizzle of BBQ sauce.", 12]]
+
 #setting up price variable - will get added to as price increases
 price = 0
-#everything the user orders will get added to this list
-order = []
-stuff_crust = []
 
-order_type, address, price = delivery(price)
-price = order_input(price)
-output_order(price)
+order_type, address, price, name = delivery(price)
+price, order, stuff_crust = order_input(price)
+confirm(order, stuff_crust, price, order_type, address, name)
