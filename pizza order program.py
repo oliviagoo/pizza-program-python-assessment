d50625c2ef8f3a802d7ab56f16ab9553260b27e3
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air-3.local>
2020-07-02 14:48:49 +1200

    Final Write-Up
    Adding the implications and Discussion documents and finishing off the project.

--- pizza order program.py
+++ pizza order program.py
@@ -5,7 +5,9 @@
 #print the welcome message
 def welcome():
     print("Welcome to Heavenly Pizza's online ordering program!")
-    print("We will guide you through the process as you decide whether to pick up your piping hot pizza or have it delivered directly to your doorstep, and choose from our delicious menu of pizzas.")
+    print("We will guide you through the process as you decide whether \
+to pick up your piping hot pizza or have it delivered directly to your\
+doorstep, and choose from our delicious menu of pizzas.")
     print()
 
 #this function forces the user to enter a whole number
@@ -30,16 +32,19 @@ def force_yn(message):
 
 #this function confirms the order and finishes the program with a final output
 def confirm(order, stuff_crust, price, order_type, address, name):
-    print("Thank you for your order! Please confirm that the following details are correct.")
+    print("Thank you for your order! Please confirm that the following \
+details are correct.")
     #confirming the pizza details
     while True:
         output_order(price, order, stuff_crust, order_type)
         correct = force_yn("Is this what you would like to order? (Y or N) ")
         if correct == "n":
-            reorder = force_yn("Are you sure you want to continue shopping? (Y or N) ")
+            reorder = force_yn("Are you sure you want to continue shopping? \
+(Y or N) ")
             print()
             if reorder == "y":
-                price, order, stuff_crust = order_input(price, order_type, order, stuff_crust)
+                price, order, stuff_crust = order_input(price, \
+                order_type, order, stuff_crust)
         else: break
     print()
     #confirming the delivery details
@@ -47,7 +52,8 @@ def confirm(order, stuff_crust, price, order_type, address, name):
         delivery_output(order_type, address, name)
         correct = force_yn("Is this correct? (Y or N) ")
         if correct == "n":
-            reorder = force_yn("Are you sure you want to change your order details? (Y or N) ")
+            reorder = force_yn("Are you sure you want to change your order \
+details? (Y or N) ")
             print()
             if reorder == "y":
                 if order_type == "d":
@@ -76,7 +82,9 @@ def delivery_output(order_type, address, name):
 def delivery(price):
     name = input("What is your name? ")
     while True:
-        answer = input("Would you like to get your pizza delivered for ${:.2f} delivery fee (D), or pick it up? (P) ".format(DELIVERY_PRICE)).lower().strip()
+        answer = input("Would you like to get your pizza delivered for \
+${:.2f} delivery fee (D), or pick it up? (P) ".format(\
+        DELIVERY_PRICE)).lower().strip()
         if answer == "d":
             price += DELIVERY_PRICE
             address = input("What is your delivery address? ")
@@ -92,7 +100,8 @@ def delivery(price):
 def edit_order(order, stuff_crust, price, order_type):
     output_order(price, order, stuff_crust, order_type)
     while True:
-        remove = force_int("Enter the number of the pizza you'd like to remove, or press 0 to quit: ") - 1
+        remove = force_int("Enter the number of the pizza you'd like to \
+remove, or press 0 to quit: ") - 1
         if order[-1] < remove:
             print("There is no pizza with that number in your order.")
         elif remove == -1:
@@ -128,11 +137,12 @@ def order_pizza(price, order, stuff_crust):
     #the restriction on how many pizzas they can order
     PIZZA_RESTRICTION = 3
     while True:
-        pizza_order = force_int("Enter the ID of the pizza you want to order, or press 0 to quit: ")
+        pizza_order = force_int("Enter the ID of the pizza you want to \
+order, or press 0 to quit: ")
         if pizza_order != 0:
             #if they've already ordered three pizzas
             if len(order) < PIZZA_RESTRICTION:
-                #if they're ordering an id lower than one and higher than the highest value
+                #making sure they order an existing pizza
                 if pizza_order > 0 and pizza_order <= gourmet_menu[-1][2]:
                     order.append(pizza_order)
                     if pizza_order < gourmet_menu[0][2]:
@@ -146,7 +156,9 @@ def order_pizza(price, order, stuff_crust):
                 else:
                     print("Please enter a pizza ID that exists.")
             else:
-                print("Unfortunately, due to the Covid-19 pandemic, you can only order a maximum of three pizzas.")
+                print("Unfortunately, due to the Covid-19 pandemic, \
+you can only order a maximum of three pizzas.")
+                print()
                 break
         else:
             print()
@@ -155,7 +167,8 @@ def order_pizza(price, order, stuff_crust):
 
 #this function asks if the user wants stuffed crust
 def stuffed_crust(price, stuff_crust):
-    answer = force_yn("Would you like to add stuffed crust for ${:.2f}? (Y or N?) ".format(STUFF_PRICE))
+    answer = force_yn("Would you like to add stuffed crust for ${:.2f}? \
+(Y or N?) ".format(STUFF_PRICE))
     if answer == "y":
         price += STUFF_PRICE
     print()
@@ -169,23 +182,27 @@ def output_order(price, order, stuff_crust, order_type):
             if order[pizza] < gourmet_menu[0][2]:
                 for option in regular_menu:
                     if option[2] == order[pizza]:
-                        print("{}) {} with stuffed crust - ${:.2f}".format(pizza + 1, option[0], REG_PRICE + STUFF_PRICE))
+                        print("{}) {} with stuffed crust - ${:.2f}".format(\
+                        pizza + 1, option[0], REG_PRICE + STUFF_PRICE))
                         break
             else:
                 for option in gourmet_menu:
                     if option[2] == order[pizza]:
-                        print("{}) {} with stuffed crust - ${:.2f}".format(pizza + 1, option[0], GOURMET_PRICE + STUFF_PRICE))
+                        print("{}) {} with stuffed crust - ${:.2f}".format(\
+                        pizza + 1, option[0], GOURMET_PRICE + STUFF_PRICE))
                         break
         else:
             if order[pizza] < gourmet_menu[0][2]:  
                 for option in regular_menu:
                     if option[2] == order[pizza]:
-                        print("{}) {} - ${:.2f}".format(pizza + 1, option[0], REG_PRICE))
+                        print("{}) {} - ${:.2f}".format(pizza + 1, option[0], \
+                        REG_PRICE))
                         break
             else:
                 for option in gourmet_menu:   
                     if option[2] == order[pizza]:
-                        print("{}) {} - ${:.2f}".format(pizza + 1, option[0], GOURMET_PRICE))
+                        print("{}) {} - ${:.2f}".format(pizza + 1, option[0], \
+                        GOURMET_PRICE))
                         break
     if order_type == "d":
         print("Delivery cost - ${:.2f}".format(DELIVERY_PRICE))
@@ -195,7 +212,9 @@ def output_order(price, order, stuff_crust, order_type):
 #this function runs the main menu for the input
 def order_input(price, order_type, order, stuff_crust):
     while True:
-        selection = input("Enter M to see the menu, P to order a pizza, O to see your order so far, E to edit your order, C to clear your order, or F to finish and place your order: ").lower().strip()
+        selection = input("Enter M to see the menu, P to order a pizza, O to \
+see your order so far, E to edit your order, C to clear your order, or F to \
+finish and place your order: ").lower().strip()
         print()
         if selection == "m":
             output_menu()
@@ -205,7 +224,8 @@ def order_input(price, order_type, order, stuff_crust):
             output_order(price, order, stuff_crust, order_type)
         elif selection == "e":
             if len(order) > 0:
-                order, stuff_crust, price = edit_order(order, stuff_crust, price, order_type)
+                order, stuff_crust, price = edit_order(order, stuff_crust, \
+                price, order_type)
             else:
                 print("You haven't ordered anything yet!")
         elif selection == "f":
@@ -213,7 +233,8 @@ def order_input(price, order_type, order, stuff_crust):
                 break
             else: print("You have to order at least one pizza")
         elif selection == "c":
-            confirmation = force_yn("Are you sure you want to clear your order? You will lose everything you have ordered so far. (Y or N?) ")
+            confirmation = force_yn("Are you sure you want to clear your order? \
+You will lose everything you have ordered so far. (Y or N?) ")
             if confirmation == "y":
                 order = []
                 stuff_crust = []
@@ -229,8 +250,23 @@ def order_input(price, order_type, order, stuff_crust):
 #main routine
 
 #menu lists - storing the pizzas the user can choose from
-regular_menu = [["Margherita", "Fresh tomato, mozzarella, fresh basil, parmesan", 1], ["Kiwi", "Bacon, egg, mozzarella", 2], ["Garlic", "Mozzarella, garlic", 3], ["Cheese", "Mozzarella, oregano", 4], ["Hawaiian", "Ham, pineapple, mozzarella", 5], ["Mediterranean (vegan)", "Lebanese herbs, olive oil, fresh tomatoes, olives, onion", 6]]
-gourmet_menu = [["Meat", "Bacon, pancetta, ham, onion, pepperoni, mozzarella", 7], ["Chicken Cranberry", "Smoked chicken, cranberry, camembert mozzarella", 8], ["Satay Chicken", "Smoked chic, onions, capsicum, pine nuts, satay sauce, mozzarella Chilli flakes and dried basil", 9], ["Big BBQ Bacon", "Smoky Bacon served on our classic marinara tomato sauce, heaped with mozzarella, topped off with a sweet and tangy BBQ drizzle", 10], ["Veggie", "Sweet red onion, mushroom, red capsicum & melting mozzarella with drizzles of our tangy roast capsicum drizzle, finished with a dash of oregano", 11], ["Meatlovers", "Spicy pepperoni, Italian sausage, succulent ham, seasoned ground beef, and crispy bacon all piled onto classic marinara sauce and finished with cheesy mozzarella and a drizzle of BBQ sauce.", 12]]
+regular_menu = [["Margherita", "Fresh tomato, mozzarella, fresh basil, \
+parmesan", 1], ["Kiwi", "Bacon, egg, mozzarella", 2], ["Garlic", \
+"Mozzarella, garlic", 3], ["Cheese", "Mozzarella, oregano", 4], \
+["Hawaiian", "Ham, pineapple, mozzarella", 5], ["Mediterranean (vegan)",\
+"Lebanese herbs, olive oil, fresh tomatoes, olives, onion", 6]]
+gourmet_menu = [["Meat", "Bacon, pancetta, ham, onion, pepperoni, \
+mozzarella", 7], ["Chicken Cranberry", "Smoked chicken, cranberry, \
+camembert mozzarella", 8], ["Satay Chicken", "Smoked chic, onions, \
+capsicum, pine nuts, satay sauce, mozzarella Chilli flakes and dried \
+basil", 9], ["Big BBQ Bacon", "Smoky Bacon served on our classic \
+marinara tomato sauce, heaped with mozzarella, topped off with a sweet \
+and tangy BBQ drizzle", 10], ["Veggie", "Sweet red onion, mushroom, \
+red capsicum & melting mozzarella with drizzles of our tangy roast \
+capsicum drizzle, finished with a dash of oregano", 11], \
+["Meatlovers", "Spicy pepperoni, Italian sausage, succulent ham, \
+seasoned ground beef, and crispy bacon all piled onto classic marinara \
+sauce and finished with cheesy mozzarella and a drizzle of BBQ sauce.", 12]]
 
 #constants - prices
 DELIVERY_PRICE = 8
